a1dada86e9d229af57896ef672e8d447e35e1f5b
gabsn <gabin.marignier@datadoghq.com>
2017-09-15 11:14:18 -0400

    wip

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 checks_path='/opt/datadog-agent6/agent/checks.d'
+datadog_config='/etc/dd-agent/datadog.yaml'
 service='nicky'
 service_path='/usr/local/bin'
 
@@ -14,14 +16,15 @@ for b in bin/*; do bins+=($b); done
 checks=()
 for c in checks/*; do checks+=($c); done
 
-#for host in ${hosts[@]}
-#do
-#	scp -C ${bins[@]} stg.$host: &
-#	scp ${checks[@]} stg.$host: &
-#done
-#
-## Wait for scp to finish
-#wait
+# Scp the files to the hosts
+for host in ${hosts[@]}
+do
+	scp -C ${bins[@]} stg.$host: &
+	scp ${checks[@]} stg.$host: &
+done
+
+# Wait for scp to finish
+wait
 
 # Trim the prefix `checks/`
 checks=("${checks[@]/checks\//}")
@@ -33,9 +36,10 @@ for i in ${!hosts[@]}
 do
 	host=${hosts[$i]}
 	bin=${bins[$i]}
+	version=${bin#$service-}
 	echo "------------------------------------------" 
 	echo "Host: $host -> $bin"
 	echo "------------------------------------------" 
 
-	ssh stg.$host BIN=$bin CHECKS=$checks CHECKS_PATH=$checks_path SERVICE=$service SERVICE_PATH=$service_path 'bash -s' < remote.sh
+	ssh stg.$host BIN=$bin CHECKS=$checks CHECKS_PATH=$checks_path DATADOG_CONFIG=$datadog_config SERVICE=$service SERVICE_PATH=$service_path VERSION=$version 'bash -s' < remote.sh
 done
--- remote.sh
+++ remote.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+datadog=datadog.yaml
 
-# First replace the checks to get all expvar metrics and resart the agent
+# Disable chef-client
+sudo chef-deviate disable "Restarting $SERVICE with the $VERSION version of dd-trace-go" 1h
+
+# Replace the checks to get all expvar metrics
 sudo cp $CHECKS $CHECKS_PATH
+
+# Use custom tags for this host
+sudo cp $DATADOG_CONFIG .
+sudo chmod 777 $datadog
+lines=$(wc -l < $datadog)
+if [ $lines > 34 ]; then
+	sudo sed -n 1,34p $datadog > temp
+	cat temp > $datadog
+fi
+sudo cat <<EOT >> $datadog
+tags:
+  - ddtracego:$VERSION
+EOT
+sudo chmod 640 $datadog
+sudo cp $datadog $DATADOG_CONFIG
+
+# Restart the agent
 sudo service datadog-agent6 restart
 
-# Second restart the bins
+# Restart the bins
 sudo service $SERVICE stop 
 sudo cp $BIN $SERVICE_PATH/$SERVICE 
 sudo service $SERVICE start
